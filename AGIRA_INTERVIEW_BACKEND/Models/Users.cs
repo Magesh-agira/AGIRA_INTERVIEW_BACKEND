c934b7c81b279efdfdb0ea1ec6e79fa861bd1ebd
Magesh-agira <magesh.v@agiratech.com>
2024-02-28 13:13:08 +0530

    Add project files.

---
+++ AGIRA_INTERVIEW_BACKEND/Models/Users.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AGIRA_INTERVIEW_BACKEND.Models
+{
+    public enum UserRole
+    {
+        SuperAdmin,
+        HR,
+        Interviewer,
+        Associate,
+        // Add more roles as needed
+    }
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
