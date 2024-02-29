620a0701dacbf5decbcc6ec66d3dbef823ec1744
Magesh-agira <magesh.v@agiratech.com>
2024-02-29 17:11:16 +0530

    hi

--- AGIRA_INTERVIEW_BACKEND/Models/Opportunities.cs
+++ AGIRA_INTERVIEW_BACKEND/Models/Opportunities.cs
@@ -1,6 +1,40 @@
-﻿namespace AGIRA_INTERVIEW_BACKEND.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AGIRA_INTERVIEW_BACKEND.Models
 {
     public class Opportunities
     {
+        public enum OpportunityStatus
+        {
+            Open,
+            Closed,
+            Hold
+        }
+
+        [Key]
+        public int OpportunityId { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        public string Location { get; set; }
+
+        public string TypeOfEmployment { get; set; }
+
+        public string Qualification { get; set; }
+
+        public decimal Salary { get; set; }
+
+        [Required]
+        public DateTime DatePosted { get; set; }
+
+        [Required]
+        public int NumberOfOpenings { get; set; }
+
+        public string Requirements { get; set; }
+
+        [Required]
+        public OpportunityStatus Status { get; set; }
     }
 }
+
